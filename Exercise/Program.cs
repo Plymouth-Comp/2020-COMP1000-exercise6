a8abcceeece4ce79e34bee2da245ead96e32cf38
Swen E Gaudl <swen.gaudl@plymouth.ac.uk>
2020-11-14 16:36:20 +0000

    Test6 started

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exercise
 {
     public class Program
     {
-        public int CalSum(int a, int b)
+        public Dictionary<string,double> RetrieveCalValues(Dictionary<string,double> data)
         {
-            return a + b;
+            // remove from the dictionary all pairs where the values are negative
+            // add a new pair to the dictionary with key="sum" as key the summation of all remaining values
+            // add a new pair to the dictionary with key="mult" as key the product of all remaining values
+            // add a new pair to the dictionary with key="div" as key the division of all remaining values
+            // round the appropriate values to 10 decimal places
+           
+            return null;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Exercise6!");
+
+            
         }
     }
 }
