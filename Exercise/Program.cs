55d700bd94ff175678e40f78449065949dd50494
Swen E Gaudl <swen.gaudl@plymouth.ac.uk>
2020-11-23 17:04:49 +0000

    exercise update

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -25,6 +25,15 @@ namespace Exercise
             // round the appropriate values to 10 decimal places
         }
 
+        public void RetrieveCalValuesRigour(Dictionary<string, double> data, string clear)
+        {
+            // remove from the dictionary all pairs where the key contains the a lexicographically version of <clear>
+            // example: clear = "corrupt" is contained in "cor rupt","corrupt", "corrupt1", "12c or ru pt45", "cor12rupt"
+            // add a new pair to the dictionary with key="sum" as key the summation of all remaining values
+            // add a new pair to the dictionary with key="mult" as key the product of all remaining values
+            // round the appropriate values to 10 decimal places
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Exercise6!");
